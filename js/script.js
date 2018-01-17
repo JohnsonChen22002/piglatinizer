4b6620126cce22a0b6265f00eb32826310f28e0e
Johnson Chen <johnsonchen12345@gmail.com>
2018-01-17 23:33:52 +0000

    PigLatinizer2.0

--- js/script.js
+++ js/script.js
@@ -5,38 +5,56 @@
 // CREATE THE FUNCTIONS BELOW
 
 // Document Ready Function. All of your jQuery should go in here. 
-$( document ).ready(function() {
-  $("button").click(function(){
+$(document).ready(function() {
+  $("button").click(function() {
     var message = $("input").val();
+    var messageSplit = sentencetoPigLatin(message);
+    
+    
+    
+    
+    
+    
+    
+    
     var playerMessage = player(message);
-  $("#output").text(playerMessage);
-  
-  
-  
-});
+    $("#output").text(playerMessage);
 
-function player(word){
- return word + "ay";    
-}
-
-function sentencetoPigLatin (sentence){
-  var i = (sentence.split('. '));  
-  console.log(sentence);
-}
-
-
+    function player(word) {
+      for (var i = 0; i < 1; i++) {
+        $("#output").append(word + "ay");
+      }
+    }
+  });
 });
 
 
+
+function sentencetoPigLatin(sentence) {
+  var words = sentence.split(' ');
+  return words;
+}
+
+function wordToPigLatin(word) {
+  var FirstLetter = word.slice(0, 1);
+  
+}
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
 
 
+function isVowel(letter) {
+  if (letter == "a" || letter == "e" || letter == "i" ||
+    letter == "o" || letter == "u") {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 
 
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
-	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
-
-
+//Loops through all the words in the sentence and transforms each word
+//It should return a transfromed sentance
